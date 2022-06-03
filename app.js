aec7f2eefd0410a62a9529cc46bc6697dd596f7e
adityasankhla01 <93070213+adityasankhla01@users.noreply.github.com>
2022-06-03 13:11:40 +0530

    userdata and tasks passed without passport

--- app.js
+++ app.js
@@ -2,34 +2,93 @@ require('dotenv').config();
 const express=require('express');
 const bodyParser=require('body-parser')
 const mongoose=require('mongoose');
+const { redirect } = require('express/lib/response');
 
+
+
+const tasks=["TaskA", "TaskB","TaskC"]
 
 const app=express();
 
-app.use(express.static('public'))
 
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.set('view engine', 'ejs')
 
-mongoose.connect("mongodb://127.0.0.1:27017/totodoDB");
+
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/totodoDB2022");
 const userSchema=new mongoose.Schema({
-
+     
+    nameOfUser:String,
     username: String,
-    password: String
+    password: String,
+    tasks:[{
+        type: String
+    }]
 })
+
+
+
+
 const User=mongoose.model("user", userSchema)
 
-app.get("/",function(req,res){
+
+
+
+app.get("/login",function(req,res){
 
     res.render('login');
 })
-app.route("/dashboard").get(function(req,res){
+app.route("/dashboard/:_id").get(function(req,res){
 
-    res.render('dashboard');
+    const userdata=req.params._id;
+
+    User.findOne({_id: userdata}, function(err,result){
+
+      if(err){
+          console.log(err);
+      }else{
+        
+        res.render("dashboard", {person:result, nameOfUser: result.nameOfUser})
+      }
+
+
+
+    })
+   
   
 
+}).post(function(req,res){
+
+   User.findOne({_id:req.params._id}, function(err,result){
+
+
+   if(err){
+
+    console.log(err)
+   }else{
+       
+    newTasks= result.tasks.push(req.body.newTask)
+     result.save();
+
+     res.redirect("/dashboard/"+req.params._id)
+      
+   }
+    
+
+
+   })
+
+
+ 
+
 })
+
+
 app.route("/register").get(function(req,res){
 
     res.render('register');
@@ -37,15 +96,53 @@ app.route("/register").get(function(req,res){
 
 }).post(function(req,res){
 
-    res.redirect("/dashboard");
+      const newUser=new User({
 
-    const newUser=new User({
+        nameOfUser:req.body.nameOfUser,
+        username:req.body.username,
+        password:req.body.password,
+        tasks:tasks
 
-        username: req.body.username,
-        password:req.body.password
-    })
+      })
+      newUser.save();
+      res.redirect("/dashboard/"+newUser._id)
+})
 
-  newUser.save();
+app.post("/login",function(req,res){
+ 
+    User.findOne({username:req.body.username},function(err,result){
+
+        if(err){
+     
+            res.send(err)
+           }else{
+        
+            if(result){
+        
+                if(result.password===req.body.password){
+        
+                    res.redirect("/dashboard/"+result._id);
+            
+                }else{
+                    res.send("Incorrect password");
+                }
+        
+                
+        
+            }else{
+                  
+
+                res.redirect("/register");
+        
+            
+            
+           }}
+            
+        
+     })
+     
+ 
+ 
 })
 app.listen(3000, function(){
 
